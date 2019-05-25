580e8455c8c369cd435a1f13a8f589b0d2c76e88
Zoha-Arif <Zoha-Arif@users.noreply.github.com>
2019-05-25 18:15:53 -0400

    WORKS CORRECTLY!!! YAAAY!

--- main.js
+++ main.js
@@ -1,26 +1,38 @@
 function calculate(startn, endn) {
     var stop = endn + 1; 
-    console.log("Stop:" + stop); 
     var currentNumber = startn; 
     var totalSum = [];  
     while (currentNumber != stop){
         var prime = true; 
-        for(var i = 0; i < currentNumber; i++){ 
-            if(startn % i == 0){
+
+        if(currentNumber == 0){
+            var prime = false; 
+        }
+
+        else if (currentNumber == 1){
+            var prime = false; 
+        }
+
+        else {
+        for(var i = 2; i < currentNumber; i++){ 
+            if(currentNumber % i == 0){
             prime = false; 
         }
         }
-        if (prime == true){
-            totalSum.push(currentNumber); 
-        }
-        currentNumber = currentNumber + 1; 
     }
+
+    if (prime == true){
+        totalSum.push(currentNumber); 
+        console.log(totalSum); 
+    }
+    currentNumber = currentNumber + 1; 
+}
 
     console.log("currentNumber" + currentNumber); 
     console.log("Prime:" + prime); 
     var sumFinal = 0; 
-    for(var x = 1; x < totalSum.length; x++){
-        sumFinal = sumFinal + totalSum[1]
+    for(var x = 0; x < totalSum.length; x++){
+        sumFinal = sumFinal + totalSum[x]; 
     }
     document.getElementById("displaySum").innerHTML = sumFinal; 
 }
@@ -28,11 +40,11 @@ function calculate(startn, endn) {
 $(document).ready(function(){ //when document is ready, function will be executed.
 
     $(document).keypress(function(key){
-
         if (key.which == 13){
-            var startn3 = document.getElementById(startn2).value; 
-            console.log(startn); 
-            var endn3 = document.getElementById(endn2).value;
+            var startn3 = document.getElementById("startn2").value; 
+            var endn3 = document.getElementById("endn2").value;
+            startn3 = parseInt(startn3); 
+            endn3 = parseInt(endn3); 
             calculate(startn3, endn3)
         }
     })
